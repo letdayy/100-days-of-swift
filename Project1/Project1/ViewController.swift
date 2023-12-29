54599e7ff8d774efacaf7736a8bc263d389ccdcd
letdayy <lcdayane49@gmail.com>
2023-12-29 15:12:19 -0300

    feat: making the image appear using UIImage

--- Project1/Project1/ViewController.swift
+++ Project1/Project1/ViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class ViewController: UITableViewController {
-    @IBOutlet var imageView: UITableView!
+    @IBOutlet weak var imageView: UIImageView!
+
     
     var pictures = [String]()
 
@@ -24,7 +25,6 @@ class ViewController: UITableViewController {
             pictures.append(item)
             }
         }
-        print(pictures)
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -35,6 +35,14 @@ class ViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
         cell.textLabel?.text = pictures[indexPath.row]
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            
+            navigationController?.pushViewController(vc, animated: true)
+        }
     }
 }
 
