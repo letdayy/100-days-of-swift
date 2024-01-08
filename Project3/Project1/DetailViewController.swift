84dad3bd0a954bc93b87272cd3b3a0a03aecd264
letdayy <lcdayane49@gmail.com>
2024-01-08 16:10:34 -0300

    feat: add share bar to the detail page and add permission to save image

--- Project3/Project1/DetailViewController.swift
+++ Project3/Project1/DetailViewController.swift
@@ -21,9 +21,10 @@ class DetailViewController: UIViewController {
             title = "Imagem \(selectedImageIndex) de \(totalImages)"
         }
         
-        
-        
         navigationItem.largeTitleDisplayMode = .never
+        
+        //colocando o botão da barra de compartilhamento na página de detalhes
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
        
 
         if let imageToLoad = selectedImage {
@@ -39,6 +40,22 @@ class DetailViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.hidesBarsOnTap = false
+    }
+    
+    //criando a função que executa a ação de abrir a barra de compartilhamento
+    @objc func shareTapped() {
+        //código que exibe as imagens da barra de compartilhamento
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8)
+        else {
+            print("Image not found")
+            return
+        }
+        
+        //código que abre a barra de compartilhamento
+        //importante colocar o image dentro do array de acitivityItems para funcionar
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
     }
 
 }
