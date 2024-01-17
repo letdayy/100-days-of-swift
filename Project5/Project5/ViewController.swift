1fb25a39c878ca6d7f1dcd2fcd6c0da48a0699e3
letdayy <lcdayane49@gmail.com>
2024-01-17 13:43:57 -0300

    feat: create the base of project

---
+++ Project5/Project5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project5
+//
+//  Created by leticia.dayane on 17/01/24.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
