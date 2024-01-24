1f7d31b456c0874a51e01e3ebe4c9133f6742593
letdayy <lcdayane49@gmail.com>
2024-01-24 16:44:56 -0300

    feat: create shopping list app

---
+++ ShoppingList/ShoppingList/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  ShoppingList
+//
+//  Created by leticia.dayane on 24/01/24.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var allItems = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Shopping List"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addItem))
+        
+        //botão que limpa a lista de compras
+        //navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(clearItems))
+
+    }
+    
+    //código de configuração da tabela
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Item", for: indexPath)
+        cell.textLabel?.text = allItems[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return allItems.count
+    }
+
+
+    @objc func addItem() {
+        let ac = UIAlertController(title: "Enter the item name", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) {
+            [weak self, weak ac] action in
+            guard let item = ac?.textFields?[0].text else { return }
+            self?.submit(item)
+        }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    func submit(_ item: String) {
+        allItems.insert(item, at: 0)
+        
+        let indexPath = IndexPath(row: 0, section: 0)
+        tableView.insertRows(at: [indexPath], with: .automatic)
+    }
+    
+    /*@objc func clearItems() {
+        
+        let confirmeTrash = UIAlertAction(title: "Are you sure you want to remove all items?", style: .default)
+        
+        
+        allItems.removeAll(keepingCapacity: true)
+        tableView.reloadData()
+    }*/
+}
+
