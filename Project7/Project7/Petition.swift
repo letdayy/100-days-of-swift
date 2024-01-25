258b65a2239fbb5a0d7e2f870678f06700c9a43b
letdayy <lcdayane49@gmail.com>
2024-01-25 13:40:17 -0300

    feat: add json file in the app using Codable

---
+++ Project7/Project7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by leticia.dayane on 25/01/24.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
