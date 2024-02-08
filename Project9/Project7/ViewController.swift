bf9112e98877ff43e2b533b6db6d35f16e691dfd
letdayy <lcdayane49@gmail.com>
2024-02-08 13:06:03 -0300

    feat: add GCD for async tasks

---
+++ Project9/Project7/ViewController.swift
@@ -0,0 +1,121 @@
+//
+//  ViewController.swift
+//  Project7
+//
+//  Created by leticia.dayane on 25/01/24.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var petitions = [Petition]()
+    //petições filtradas
+    var filteredPetitions = [Petition]()
+    var isFiltereing = false
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .bookmarks, target: self, action: #selector(showCredits))
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(filterPetition))
+        
+        let urlString: String
+        
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
+        
+        DispatchQueue(label: "com.example.queue", qos: .userInitiated).async {
+            [weak self] in
+            if let url = URL(string: urlString) {
+                if let data = try? Data(contentsOf: url) {
+                    self?.parse(json: data)
+                    return
+                }
+            }
+            
+            self?.showError()
+        }
+    }
+    
+    @objc func filterPetition() {
+        let ac = UIAlertController(title: "Filter petitions", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let filterAction = UIAlertAction(title: "Filter", style: .default) {
+            [weak self, weak ac] action in
+            guard let message = ac?.textFields?[0].text else { return }
+            self?.filterPetitions(message)
+            
+        
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        ac.addAction(filterAction)
+        ac.addAction(cancelAction)
+        
+        present(ac, animated: true)
+    }
+    
+    func filterPetitions(_ message: String) {
+        if message.isEmpty {
+            isFiltereing = false
+            tableView.reloadData()
+            return
+        }
+        
+        filteredPetitions = petitions.filter { $0.title.contains(message)}
+        isFiltereing = true
+        tableView.reloadData()
+    }
+    
+    @objc func showCredits() {
+        let ac = UIAlertController(title: "Credits", message: "The data comes from Whitehouse`s We The People API", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Ok!", style: .default))
+        present(ac, animated: true)
+    }
+    
+    func showError() {
+        DispatchQueue.main.async {
+            let ac = UIAlertController(title: "Loading error", message: "There is a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            self.present(ac, animated: true)
+        }
+    }
+    
+    func parse(json: Data) {
+        let decoder = JSONDecoder()
+        
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
+            petitions = jsonPetitions.results
+            
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return isFiltereing ? filteredPetitions.count : petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        let petition = isFiltereing ? filteredPetitions[indexPath.row] : petitions[indexPath.row]
+        cell.textLabel?.text = petition.title
+        cell.detailTextLabel?.text = petition.body
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        vc.detailItem = petitions[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
+    }
+}
+
