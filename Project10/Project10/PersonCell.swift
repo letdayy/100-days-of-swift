274ffbd9cfda6602ff285abc6d33668f71f17855
letdayy <lcdayane49@gmail.com>
2024-02-16 16:33:55 -0300

    feat: base of the project using collection view

---
+++ Project10/Project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by leticia.dayane on 16/02/24.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIView!
+    @IBOutlet weak var name: UILabel!
+    
+}
