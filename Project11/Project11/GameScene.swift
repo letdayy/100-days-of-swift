73e5db9f75ac5c56c5cdcf3b9981c6ed3f6c039d
letdayy <lcdayane49@gmail.com>
2024-02-23 15:43:26 -0300

    feat: make the box a physical body

--- Project11/Project11/GameScene.swift
+++ Project11/Project11/GameScene.swift
@@ -16,6 +16,8 @@ class GameScene: SKScene {
         background.blendMode = .replace
         background.zPosition = -1 //para deixar o plano de fundo atrás sempre
         addChild(background)
+        //colocando um peso na caixa
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -24,6 +26,7 @@ class GameScene: SKScene {
             let box = SKSpriteNode(color: UIColor.red, size: CGSize(width: 64, height: 64))
             box.position = location
             addChild(box)
+            box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 64, height: 64))
         }
     }
 }
