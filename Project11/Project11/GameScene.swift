dad40c5b96981503b24a18f3b3dad45489d41730
letdayy <lcdayane49@gmail.com>
2024-02-23 15:59:11 -0300

    feat: add a bouncer

--- Project11/Project11/GameScene.swift
+++ Project11/Project11/GameScene.swift
@@ -18,6 +18,12 @@ class GameScene: SKScene {
         addChild(background)
         //colocando um peso na caixa
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        
+        let bouncer = SKSpriteNode(imageNamed: "bouncer")
+        bouncer.position = CGPoint(x: 512, y: 0)
+        bouncer.physicsBody = SKPhysicsBody(circleOfRadius: bouncer.size.width / 2.0)
+        bouncer.physicsBody?.isDynamic = false
+        addChild(bouncer)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -25,6 +31,7 @@ class GameScene: SKScene {
             let location = touch.location(in: self)
             let ball = SKSpriteNode(imageNamed: "ballRed")
             ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
+            ball.physicsBody?.restitution = 0.4
             ball.position = location
             addChild(ball)
         }
