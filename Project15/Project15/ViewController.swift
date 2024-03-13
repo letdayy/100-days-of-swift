9c84e198aa323657aea2b5fba3e1d6bed257a2c9
letdayy <lcdayane49@gmail.com>
2024-03-13 16:35:48 -0300

    feat: add animations

--- Project15/Project15/ViewController.swift
+++ Project15/Project15/ViewController.swift
@@ -39,10 +39,33 @@ class ViewController: UIViewController {
     @IBAction func tapped(_ sender: UIButton) {
         sender.isHidden = true
         
-        UIView.animate(withDuration: 1, delay: 0, animations: {
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 3, animations: {
             switch self.currentAnimation {
             case 0:
-                break
+                self.imageView.transform = CGAffineTransform(scaleX: 2, y: 2)
+                
+            case 1:
+                self.imageView.transform = .identity
+                
+            case 2:
+                self.imageView.transform = CGAffineTransform(translationX: -256, y: -200)
+                
+            case 3:
+                self.imageView.transform = .identity
+                
+            case 4:
+                self.imageView.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+                
+            case 5:
+                self.imageView.transform = .identity
+                
+            case 6:
+                self.imageView.alpha = 0.1
+                self.imageView.backgroundColor = UIColor.white
+                
+            case 7:
+                self.imageView.alpha = 1
+                self.imageView.backgroundColor = UIColor.clear
             
             default:
                 break
