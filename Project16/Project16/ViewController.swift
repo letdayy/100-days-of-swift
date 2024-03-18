2c2899271ab668d42716cb52a6c2f81c0d4afe96
letdayy <lcdayane49@gmail.com>
2024-03-18 16:11:34 -0300

    feat: add class capital

--- Project16/Project16/ViewController.swift
+++ Project16/Project16/ViewController.swift
@@ -5,10 +5,12 @@
 //  Created by leticia.dayane on 18/03/24.
 //
 
+import MapKit
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
