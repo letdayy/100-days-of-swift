4862c571d953e727899d84944c90abbb876d3fa7
letdayy <lcdayane49@gmail.com>
2024-03-20 15:28:10 -0300

    feat: base configuration

---
+++ Project17/Project17/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  Project17
+//
+//  Created by leticia.dayane on 20/03/24.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
