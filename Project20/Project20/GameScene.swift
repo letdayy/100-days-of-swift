3297b5031a9faa19c394b34296f63f2afb73f188
letdayy <lcdayane49@gmail.com>
2024-04-01 18:48:01 -0300

    feat: base of project and add background

---
+++ Project20/Project20/GameScene.swift
@@ -0,0 +1,37 @@
+//
+//  GameScene.swift
+//  Project20
+//
+//  Created by leticia.dayane on 01/04/24.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    var gameTimer: Timer?
+    var fireworks = [SKNode]()
+    
+    let leftEdge = -22
+    let bottomEdge = -22
+    let rightEdge = 1024 + 22
+    
+    var score = 0 {
+        didSet {
+            //code here
+        }
+    }
+    
+    override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "background")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
+        
+        gameTimer = Timer.scheduledTimer(timeInterval: 6, target: self, selector: #selector(launchFireworks), userInfo: nil, repeats: true)
+    }
+    
+    @objc func launchFireworks() {
+        
+    }
+}
