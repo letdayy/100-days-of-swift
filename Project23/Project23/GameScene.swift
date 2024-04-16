0c30c5c1a89df54eff5e81ee31b3ca63b474807d
letdayy <lcdayane49@gmail.com>
2024-04-16 14:02:00 -0300

    feat: project base

---
+++ Project23/Project23/GameScene.swift
@@ -0,0 +1,14 @@
+//
+//  GameScene.swift
+//  Project23
+//
+//  Created by leticia.dayane on 16/04/24.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        
+    }
+}
