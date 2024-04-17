afe7efe791a2a766d64219c81b155ed3c9441132
letdayy <lcdayane49@gmail.com>
2024-04-17 15:59:02 -0300

    feat: create background, score and lives images

--- Project23/Project23/GameScene.swift
+++ Project23/Project23/GameScene.swift
@@ -8,7 +8,58 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    var gameScore: SKLabelNode!
+    var score = 0 {
+        didSet {
+            gameScore.text = "Score: \(score)"
+        }
+    }
+    
+    //vidas do jogo
+    var livesImages = [SKSpriteNode]()
+    var lives = 3
+    
     override func didMove(to view: SKView) {
+        
+        let background = SKSpriteNode(imageNamed: "sliceBackground")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.scale(to: CGSize(width: size.width, height: size.height))
+        background.zPosition = -1
+        addChild(background)
+        
+        physicsWorld.gravity = CGVector(dx: 0, dy: -6)
+        physicsWorld.speed = 0.85
+        
+        createScore()
+        createLives()
+        createSlices()
+    }
+    
+    func createScore() {
+        gameScore = SKLabelNode(fontNamed: "Chalkduster")
+        gameScore.horizontalAlignmentMode = .left
+        gameScore.fontSize = 48
+        gameScore.fontColor = .black
+        addChild(gameScore)
+        
+        gameScore.position = CGPoint(x: 8, y: 8)
+        score = 0
+    }
+    
+    func createLives() {
+        for i in 0 ..< 3 {
+            let spriteNode = SKSpriteNode(imageNamed: "sliceLife")
+            spriteNode.position = CGPoint(x: CGFloat(834 + (i * 70)), y: 720)
+            spriteNode.color = .black
+            spriteNode.colorBlendFactor = 1.0
+            addChild(spriteNode)
+            
+            livesImages.append(spriteNode)
+        }
+    }
+    
+    func createSlices() {
         
     }
 }
