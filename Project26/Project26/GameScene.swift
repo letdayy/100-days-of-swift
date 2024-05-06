1d905c56a8c41911f9bfd577fce626d9cdc173e9
letdayy <lcdayane49@gmail.com>
2024-05-06 17:55:02 -0300

    feat: add project base

---
+++ Project26/Project26/GameScene.swift
@@ -0,0 +1,19 @@
+//
+//  GameScene.swift
+//  Project26
+//
+//  Created by leticia.dayane on 06/05/24.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    func loadLevel() {
+        
+    }
+}
