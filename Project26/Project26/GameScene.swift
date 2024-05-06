bdc2d05df0f0c792bdffe51e540f9104ee9a451d
letdayy <lcdayane49@gmail.com>
2024-05-06 18:00:03 -0300

    feat: add code that reads the letters

--- Project26/Project26/GameScene.swift
+++ Project26/Project26/GameScene.swift
@@ -14,6 +14,34 @@ class GameScene: SKScene {
     }
     
     func loadLevel() {
+        guard let levelURL = Bundle.main.url(forResource: "level1", withExtension: "txt") else {
+            fatalError("Could not find level1.txt in the app bundle.")
+        }
+        guard let levelString = try? String(contentsOf: levelURL) else {
+            fatalError("Could not load level1.txt in the app bundle.")
+        }
+        
+        let lines = levelString.components(separatedBy: "\n")
+        
+        for (row, line) in lines.reversed().enumerated() {
+            for (column, letter) in line.enumerated() {
+                let position = CGPoint(x: (64 * column) + 32, y: (64 * row) + 32)
+                
+                if letter == "x" {
+                    // load wall
+                } else if letter == "v" {
+                    // load vortex
+                } else if letter == "s" {
+                    // load star
+                } else if letter == "f" {
+                    // load finish
+                } else if letter == " " {
+                    // do nothing!
+                } else {
+                    fatalError("Unknown level letter: \(letter)")
+                }
+            }
+        }
         
     }
 }
