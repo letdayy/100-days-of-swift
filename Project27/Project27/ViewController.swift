db1c85ae33f391b84bf62f673cede5aa83b00e2e
letdayy <lcdayane49@gmail.com>
2024-05-10 17:03:20 -0300

    feat: draw red rectangle

--- Project27/Project27/ViewController.swift
+++ Project27/Project27/ViewController.swift
@@ -10,13 +10,45 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     
+    var currentDrawType = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        drawRetangle()
+    }
+    
+    func drawRetangle() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        
+        let img = renderer.image { ctx in
+            let rectangle = CGRect(x: 0, y: 0, width: 512, height: 512)
+            
+            ctx.cgContext.setFillColor(UIColor.red.cgColor)
+            ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+            ctx.cgContext.setLineWidth(10)
+            
+            ctx.cgContext.addRect(rectangle)
+            ctx.cgContext.drawPath(using: .fillStroke)
+        }
+        
+        imageView.image = img
     }
 
     @IBAction func redrawTapped(_ sender: Any) {
+        currentDrawType += 1
         
+        if currentDrawType > 5 {
+            currentDrawType = 0
+        }
+        
+        switch currentDrawType {
+        case 0:
+            drawRetangle()
+            
+        default:
+            break
+        }
     }
     
 }
