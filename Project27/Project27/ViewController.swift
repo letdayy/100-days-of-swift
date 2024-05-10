55a018c5d472d0e71211a4bfc9c56cf969893c31
letdayy <lcdayane49@gmail.com>
2024-05-10 16:56:52 -0300

    feat: add base of project

---
+++ Project27/Project27/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Project27
+//
+//  Created by leticia.dayane on 10/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func redrawTapped(_ sender: Any) {
+        
+    }
+    
+}
+
