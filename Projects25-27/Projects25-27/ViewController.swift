421b9eef796405dfa6a494b4330cb35fc7af7bbe
letdayy <lcdayane49@gmail.com>
2024-05-14 17:04:26 -0300

    feat: add a top title

--- Projects25-27/Projects25-27/ViewController.swift
+++ Projects25-27/Projects25-27/ViewController.swift
@@ -45,7 +45,52 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         currentImage = image
     }
+    
+    func setTopTitle(image: UIImage, string: String) -> UIImage {
+        let renderer = UIGraphicsImageRenderer(size: image.size)
+        
+        let renderedImage = renderer.image { ctx in
+            image.draw(at: CGPoint(x: 0, y: 0))
+            
+            let paragraphStyle = NSMutableParagraphStyle()
+            paragraphStyle.alignment = .center
+            
+            let attrs: [NSAttributedString.Key: Any] = [
+                .foregroundColor: UIColor.black,
+                .font: UIFont(name: "HelveticaNeue", size: 100)!,
+                .paragraphStyle: paragraphStyle
+            ]
+            
+            let margin = 32
+            let textWidth = Int(image.size.width) - (margin * 2)
+            let textHeight = Int(image.size.height) - (margin * 2)
+            let textRect = CGRect(x: margin, y: margin, width: textWidth, height: textHeight)
+            string.draw(in: textRect, withAttributes: attrs)
+        }
+        
+        return renderedImage
+    }
 
-
+    @IBAction func setTopAction(_ sender: Any) {
+        guard let image = currentImage else { return }
+        
+        let ac = UIAlertController(title: "Add a set text", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        ac.addAction(UIAlertAction(title: "OK", style: .default, handler: {[weak self] _ in
+            guard let self = self, var string = ac.textFields?.first?.text else { return }
+            self.currentImage = self.setTopTitle(image: image, string: string)
+            self.imageView.image = self.currentImage
+        }))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
+    }
+    
+    @IBAction func setBottomAction(_ sender: Any) {
+        let ac = UIAlertController(title: "Add a bottom text", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
+    }
 }
 
