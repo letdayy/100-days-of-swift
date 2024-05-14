eabe63e8ced17c3a1357429d667371c7f9838c28
letdayy <lcdayane49@gmail.com>
2024-05-14 16:28:25 -0300

    feat: import image

--- Projects25-27/Projects25-27/ViewController.swift
+++ Projects25-27/Projects25-27/ViewController.swift
@@ -7,11 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var importButton: UIButton!
     @IBOutlet weak var setTopButton: UIButton!
     @IBOutlet weak var setBottomButton: UIButton!
+    
+    var currentImage: UIImage!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,10 +21,29 @@ class ViewController: UIViewController {
         centerTitleButton(button: importButton)
         centerTitleButton(button: setTopButton)
         centerTitleButton(button: setBottomButton)
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
     }
     
     func centerTitleButton(button: UIButton) {
         button.titleLabel?.textAlignment = .center
+    }
+    
+    @objc func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        
+        imageView.image = image
+        
+        dismiss(animated: true)
+        
+        currentImage = image
     }
 
 
