89c3c641a2f0d94625beaf83f11213a6a0137cde
letdayy <lcdayane49@gmail.com>
2024-05-17 19:05:12 -0300

    feat: add text editor

--- Project28/Project28/ViewController.swift
+++ Project28/Project28/ViewController.swift
@@ -13,9 +13,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = "Nothing to see here"
+        
         let notificationCenter = NotificationCenter.default
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(saveSecretMessage), name: UIApplication.willResignActiveNotification, object: nil)
     }
 
     @objc func adjustForKeyboard(notification: Notification) {
@@ -35,8 +38,25 @@ class ViewController: UIViewController {
         let selectedRange = secret.selectedRange
         secret.scrollRangeToVisible(selectedRange)
     }
+    
+    func unlockSecretMessage() {
+        secret.isHidden = false
+        title = "Secret stuff!"
+        
+        secret.text = KeychainWrapper.standard.string(forKey: "SecretMessage") ?? ""
+    }
+    
+    @objc func saveSecretMessage() {
+        guard secret.isHidden == false else { return }
+        
+        KeychainWrapper.standard.set(secret.text, forKey: "SecretMessage")
+        secret.resignFirstResponder()
+        secret.isHidden = true
+        title = "Nothing to see here"
+    }
 
     @IBAction func authenticateTapped(_ sender: Any) {
+        unlockSecretMessage()
     }
 }
 
