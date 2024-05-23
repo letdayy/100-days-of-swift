3e1a75fee779163290463f501ec116d189fe6c9f
letdayy <lcdayane49@gmail.com>
2024-05-23 17:35:48 -0300

    feat: base of project

---
+++ Project29/Project29/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  Project29
+//
+//  Created by leticia.dayane on 23/05/24.
+//
+
+import SpriteKit
+
+enum CollisionTypes: UInt32 {
+    case banana = 1
+    case building = 2
+    case player = 4
+}
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+       
+    }
+}
