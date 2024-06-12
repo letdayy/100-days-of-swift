5f0c0ad9f2586ee7f6f6e1d81ca75862ca55e523
letdayy <lcdayane49@gmail.com>
2024-06-12 15:36:44 -0300

    feat: add base of project

---
+++ Projects28-30/Projects28-30/MemoryCell.swift
@@ -0,0 +1,13 @@
+//
+//  MemoryCell.swift
+//  Projects28-30
+//
+//  Created by leticia.dayane on 12/06/24.
+//
+
+import UIKit
+
+class MemoryCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
---
+++ Projects28-30/Projects28-30/ViewController.swift
@@ -0,0 +1,80 @@
+//
+//  ViewController.swift
+//  Projects28-30
+//
+//  Created by leticia.dayane on 12/06/24.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+    
+    var cards = [UIImage]()
+    var flippedIndices = [IndexPath]()
+    var matchedCards = Set<IndexPath>()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        setupCards()
+    }
+    
+    func setupCards() {
+        let cardImages = ["card1", "card2", "card3", "card4"].compactMap { UIImage(named: $0)}
+        cards = cardImages + cardImages
+        cards.shuffle()
+    }
+    
+    func checkForMatch() {
+        guard flippedIndices.count == 2 else { return }
+        
+        let firstCard = cards[flippedIndices[0].item]
+        let secondCard = cards[flippedIndices[1].item]
+        
+        if firstCard == secondCard {
+            matchedCards.insert(flippedIndices[0])
+            matchedCards.insert(flippedIndices[1])
+        } else {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                for indexPath in self.flippedIndices {
+                    if let cell = self.collectionView.cellForItem(at: indexPath) as? MemoryCell {
+                        cell.imageView.image = UIImage(named: "back")
+                    }
+                }
+            }
+        }
+        flippedIndices.removeAll()
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        cards.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MemoryCell", for: indexPath) as! MemoryCell
+        let cardImage = matchedCards.contains(indexPath) ? cards[indexPath.item] : UIImage(named: "back")
+        cell.imageView.image = cardImage
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if flippedIndices.contains(indexPath) || matchedCards.contains(indexPath) {
+            return
+        }
+        
+        flippedIndices.append(indexPath)
+        
+        let cell = collectionView.cellForItem(at: indexPath) as! MemoryCell
+        cell.imageView.image = cards[indexPath.item]
+        
+        if flippedIndices.count == 2 {
+            checkForMatch()
+        }
+
+    }
+}
+
