52fc0aafde03750840b99c9ceb350409ed50ec44
letdayy <lcdayane49@gmail.com>
2024-06-15 01:57:09 -0300

    feat: select two cards and flip

--- Projects28-30/Projects28-30/ViewController.swift
+++ Projects28-30/Projects28-30/ViewController.swift
@@ -9,21 +9,10 @@ import UIKit
 
 class ViewController: UICollectionViewController {
     @IBOutlet weak var imageView: UIImageView!
-    
-    /*var backCard = UIImage(named: "back")
-    var images: [UIImage] = [UIImage(named: "card1")!, UIImage(named: "card2")!, UIImage(named: "card3")!, UIImage(named: "card4")!, UIImage(named: "card1")!, UIImage(named: "card2")!, UIImage(named: "card3")!, UIImage(named: "card4")!].shuffled()
-    var alternateImages: [UIImage] = [UIImage(named: "card1")!, UIImage(named: "card2")!, UIImage(named: "card3")!, UIImage(named: "card4")!, UIImage(named: "card1")!, UIImage(named: "card2")!, UIImage(named: "card3")!, UIImage(named: "card4")!].shuffled()
-    var currentImages: [UIImage]*/
     var currentImageNames: [String] = []
     let backImageName = "back"
     let alternateImageNames = ["card1", "card2", "card3", "card4", "card1", "card2", "card3", "card4"].shuffled()
-    
-    let backImage = UIImage(named: "back")
-    
-    /*required init?(coder: NSCoder) {
-        currentImages = images
-        super.init(coder: coder)
-    }*/
+    var selectedIndices: [IndexPath] = [] //array para armazenar quantos itens foram selecionados
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,33 +31,39 @@ class ViewController: UICollectionViewController {
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MemoryCell", for: indexPath) as? MemoryCell else { fatalError() }
-        //cell.imageView.image = currentImages[indexPath.item]
-        //cell.imageView.image = backImage
         
         let imageName = currentImageNames[indexPath.item]
         cell.imageView.image = UIImage(named: imageName)
-        
-        /*for currentImage in currentImages {
-            cell.imageView.image = backImage
-        }*/
         
         return cell
     }
     
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if selectedIndices.contains(indexPath) {
+            return
+        }
+        
+        selectedIndices.append(indexPath)
         
         if currentImageNames[indexPath.item] == backImageName {
             currentImageNames[indexPath.item] = alternateImageNames[indexPath.item]
         } else {
             currentImageNames[indexPath.item] = backImageName
         }
-        
-        /*if currentImages[indexPath.item] == images[indexPath.item] {
-            currentImages[indexPath.item] = alternateImages[indexPath.item]
-        } else {
-            currentImages[indexPath.item] = images[indexPath.item]
-        }*/
         collectionView.reloadItems(at: [indexPath])
+        
+        if selectedIndices.count == 2 {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [weak self] in
+                guard let self = self else { return }
+                
+                for index in self.selectedIndices {
+                    self.currentImageNames[index.item] = self.backImageName
+                }
+                
+                self.collectionView.reloadItems(at: self.selectedIndices)
+                self.selectedIndices.removeAll()
+            }
+        }
     }
 }
 
