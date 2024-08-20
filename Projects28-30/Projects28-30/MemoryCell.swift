daa1f5150aa7041df2e4eb5d4a57473145c82e59
letdayy <lcdayane49@gmail.com>
2024-08-20 19:03:50 -0300

    fix: add alterations to project

---
+++ Projects28-30/Projects28-30/MemoryCell.swift
@@ -0,0 +1,12 @@
+//
+//  MemoryCell.swift
+//  Projects28-30
+//
+//  Created by leticia.dayane on 14/06/24.
+//
+
+import UIKit
+
+class MemoryCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+}
