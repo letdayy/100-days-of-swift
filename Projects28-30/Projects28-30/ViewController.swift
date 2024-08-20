a70cb42a18da750a17308e2a0c012330abaf106a
letdayy <lcdayane49@gmail.com>
2024-08-20 19:03:50 -0300

    docs: add images to assets

---
+++ Projects28-30/Projects28-30/ViewController.swift
@@ -0,0 +1,125 @@
+//
+//  ViewController.swift
+//  Projects28-30
+//
+//  Created by leticia.dayane on 14/06/24.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    var currentImageNames: [String] = []
+    let backImageName = "back"
+    var alternateImageNames = ["card1", "card2", "card3", "card4", "card1", "card2", "card3", "card4"].shuffled()
+    var selectedIndices: [IndexPath] = [] //array para armazenar quantos itens foram selecionados
+    var matchedIndices: [IndexPath] = [] //array para armazenar cartas iguais
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        currentImageNames = Array(repeating: backImageName, count: alternateImageNames.count)
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "New Game", style: .plain, target: self, action: #selector(newGame))
+    }
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return currentImageNames.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MemoryCell", for: indexPath) as? MemoryCell else { fatalError() }
+        
+        let imageName = currentImageNames[indexPath.item]
+        cell.imageView.image = UIImage(named: imageName)
+        
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if selectedIndices.contains(indexPath) || matchedIndices.contains(indexPath) {
+            return
+        }
+        
+        selectedIndices.append(indexPath)
+        
+        if currentImageNames[indexPath.item] == backImageName {
+            currentImageNames[indexPath.item] = alternateImageNames[indexPath.item]
+        } else {
+            currentImageNames[indexPath.item] = backImageName
+        }
+        collectionView.reloadItems(at: [indexPath])
+        
+        if selectedIndices.count == 2 {
+            let firstIndex = selectedIndices[0]
+            let secondIndex = selectedIndices[1]
+            let firstImageName = currentImageNames[firstIndex.item]
+            let secondImageName = currentImageNames[secondIndex.item]
+            
+            if firstImageName == secondImageName {
+                matchedIndices.append(contentsOf: selectedIndices)
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [weak self] in
+                    guard let self = self else { return }
+                    
+                    equalsCards(firstImageName: firstImageName, secondImageName: secondImageName)
+                    
+                    //se todas as cartas forem encontradas
+                    if self.matchedIndices.count == self.currentImageNames.count {
+                        self.showAlert()
+                    }
+                }
+                
+            } else {
+                diferentCards()
+            }
+        }
+    }
+    
+    func equalsCards(firstImageName: String, secondImageName: String) {
+            for index in self.selectedIndices {
+                self.currentImageNames[index.item] = ""
+            }
+            
+            self.collectionView.reloadItems(at: self.selectedIndices)
+            self.selectedIndices.removeAll()
+    }
+    
+    func diferentCards() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [weak self] in
+            guard let self = self else { return }
+            
+            for index in self.selectedIndices {
+                self.currentImageNames[index.item] = self.backImageName
+            }
+            
+            self.collectionView.reloadItems(at: self.selectedIndices)
+            self.selectedIndices.removeAll()
+        }
+    }
+    
+    func showAlert() {
+        let ac = UIAlertController(title: "Congratulations!", message: "You finished the game", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default) {_ in
+            self.newGame()
+        })
+        
+        DispatchQueue.main.async {
+            self.present(ac, animated: true)
+        }
+    }
+    
+    @objc func newGame() {
+        alternateImageNames.shuffle()
+        currentImageNames = Array(repeating: backImageName, count: alternateImageNames.count)
+        selectedIndices.removeAll()
+        matchedIndices.removeAll()
+        collectionView.reloadData()
+    }
+}
+
